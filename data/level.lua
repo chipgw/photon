d39fcf298c8ad026c92ba5fe600769706a5c9b0b
chipgw <gw.chip.gw@gmail.com>
2013-05-03 10:55:36 -0500

    added comments to level.lua fixed spelling of "congratulations"

--- data/level.lua
+++ data/level.lua
@@ -1,15 +1,20 @@
+-- sets the popup message with a timeout of 4 seconds.
 photon.gui.set_message("first I want you to gather 12 tnt...", 4)
 
+-- sets the photon's location.
 photon.player.set_location(5, 2)
 
+-- victory condition function returns > 0 on victory, < 0 on defeat and 0 otherwise.
 photon.level.set_victory_condition(function()
     if  photon.player.get_item_count("tnt") >= 12 then
+        -- enables free movement.
         photon.player.snap_to_beam(false)
         photon.gui.set_message("good work! now destroy all the targets...", 4)
-        --
+
+        -- sets a new victory condition. (objective 2)
         photon.level.set_victory_condition(function()
             if  photon.level.get_item_count("target") < 1 then
-                photon.gui.set_message("congradulations you have won!\nnow leave the level using a reciever.", 4)
+                photon.gui.set_message("congratulations you have won!\nnow leave the level using a reciever.", 4)
                 return 1;
             end
             return 0

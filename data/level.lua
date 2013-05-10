89d99dc5a92154346542ce27ced9b0bee2b47980
chipgw <gw.chip.gw@gmail.com>
2013-05-10 08:21:13 -0500

    modified the default level.

--- data/level.lua
+++ data/level.lua
@@ -2,15 +2,18 @@
 photon.gui.set_message("Welcome to Photon " .. photon.build.version() .. "!", 4)
 
 photon.after(function()
-    photon.gui.set_message("first I want you to gather 12 tnt...", 4)
+    photon.gui.set_message("first I want you to gather 20 tnt...", 4)
 end, 3)
 
 -- sets the photon's location.
-photon.player.set_location(5, 2)
+photon.player.set_location(2, 2)
+
+halfway_message_shown = false
 
 -- victory condition function returns > 0 on victory, < 0 on defeat and 0 otherwise.
 photon.level.set_victory_condition(function()
-    if  photon.player.get_item_count("tnt") >= 12 then
+    local tnt_count = photon.player.get_item_count("tnt")
+    if tnt_count >= 20 then
         -- enables free movement.
         photon.player.snap_to_beam(false)
         photon.gui.set_message("good work! now destroy all the targets...", 4)
@@ -23,6 +26,9 @@ photon.level.set_victory_condition(function()
             end
             return 0
         end)
+    elseif not halfway_message_shown and tnt_count >= 10 then
+        photon.gui.set_message("halfway there...", 4)
+        halfway_message_shown = true
     end
     return 0
 end)
